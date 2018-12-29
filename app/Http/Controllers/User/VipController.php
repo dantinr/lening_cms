1526c5514e8991127d7d81e5c0f3d4ad30775dc6
liwei <dantinr@qq.com>
2018-12-30 07:27:36 +0800

    init

---
+++ app/Http/Controllers/User/VipController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VipController extends Controller
+{
+    //
+	public $name = null;
+	public $age = 55;
+}
